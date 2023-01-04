fd53f07b6d63faa9922a82ef1eca9f64f6b2112e
wrijlarr <wrijlarr@amazon.com>
2023-01-04 21:15:45 +0000

    join and split

---
+++ lab_18.py
@@ -0,0 +1,11 @@
+def main():
+
+ user_name = input("what is your name? ")
+ 
+ dow_input= input("what day of the week is it? ")
+
+ print("Hello" + user_name + "! It is " + dow_input)
+
+ 
+ if __name__ == "__main__":
+     main()

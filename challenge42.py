fc5e4c707a7be490b66444da36b9629e9d14955d
wrijlarr <wrijlarr@amazon.com>
2023-01-05 20:34:21 +0000

    Learning about conditional if

---
+++ challenge42.py
@@ -0,0 +1,11 @@
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+print(f"My {challeng[2] [1]}! The {challlrnge[2] [0]}
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+
+
